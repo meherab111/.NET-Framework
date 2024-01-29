e1eee17b882bd0f270211a41b180d1cb0d032557
meherab111 <mehrabborno@gmail.com>
2024-01-30 02:39:04 +0600

    HELLO WORLD

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+
+        public ActionResult Personal_Info()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+    }
+}
